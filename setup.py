91dbcbe5e1ee56f9e2a2c7f7d9834ed6ff714a84
Mike Eheler <meheler@blizzard.com>
2020-04-14 10:43:58 -0700

    Add PyPI long description

--- setup.py
+++ setup.py
@@ -8,7 +8,8 @@ import sys
 import setuptools
 import heroprotocol.build
 
-install_requires = ['mpyq >= 0.2.5']
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
 
 setuptools.setup(
     name='heroprotocol',
@@ -17,9 +18,11 @@ setuptools.setup(
     author_email='HeroesReplays@blizzard.com',
     url='https://github.com/Blizzard/heroprotocol',
     description='Python library to decode Heroes of the Storm replays',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=[
         'heroprotocol',
-        'heroprotocol.versions'
+        'heroprotocol.versions',
     ],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
@@ -28,19 +31,17 @@ setuptools.setup(
         'Intended Audience :: End Users/Desktop',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
         'Topic :: Games/Entertainment :: Real Time Strategy',
         'Topic :: Software Development :: Libraries',
         'Topic :: System :: Archiving',
         'Topic :: Utilities',
     ],
-    install_requires=install_requires,
+    install_requires=['mpyq >= 0.2.5'],
     entry_points={
         'console_scripts': [
             'heroprotocol = heroprotocol.heroprotocol:main',
-            'hero_cli.py = heroprotocol.hero_cli:main',
         ]
     },
-    python_requires='>=2.6'
+    python_requires='>=3'
 )
