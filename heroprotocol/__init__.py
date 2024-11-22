856bd76929e5427e9ab12268c92925c8a571ec54
Heroes Replays <HeroesReplays@blizzard.com>
2024-11-21 18:19:09 -0800

    Generated replay parser for Heroes of the Storm 2.55.8.93269

--- heroprotocol/__init__.py
+++ heroprotocol/__init__.py
@@ -5,7 +5,7 @@
 #
 
 __author__ = 'Blizzard Entertainment'
-__version__ = (2, 55, 8, 93229)
+__version__ = (2, 55, 8, 93269)
 
 from pkgutil import extend_path
 
--- heroprotocol/build.py
+++ heroprotocol/build.py
@@ -8,7 +8,7 @@ import subprocess
 
 
 def game_version():
-    return '2.55.8.93229'
+    return '2.55.8.93269'
 
 
 def read_command_output(cmd):
